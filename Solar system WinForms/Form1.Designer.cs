030ad9854fb51855c7889ce012a78fb4a545d909
DimaStill <dimastill123@gmail.com>
2018-10-25 20:41:12 +0300

    Create Solar System

--- Solar system WinForms/Form1.Designer.cs
+++ Solar system WinForms/Form1.Designer.cs
@@ -28,13 +28,271 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mercuryImage = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.venusImage = new System.Windows.Forms.PictureBox();
+            this.earthImage = new System.Windows.Forms.PictureBox();
+            this.marsImage = new System.Windows.Forms.PictureBox();
+            this.jupiterImage = new System.Windows.Forms.PictureBox();
+            this.saturnImage = new System.Windows.Forms.PictureBox();
+            this.uranusImage = new System.Windows.Forms.PictureBox();
+            this.neptunImage = new System.Windows.Forms.PictureBox();
+            this.plutonImage = new System.Windows.Forms.PictureBox();
+            this.moonImage = new System.Windows.Forms.PictureBox();
+            this.phobosImage = new System.Windows.Forms.PictureBox();
+            this.deimosImage = new System.Windows.Forms.PictureBox();
+            this.ioImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mercuryImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.venusImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.earthImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marsImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jupiterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturnImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uranusImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.neptunImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plutonImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moonImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phobosImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deimosImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ioImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Solar_system_WinForms.Properties.Resources.Sun;
+            this.pictureBox1.Location = new System.Drawing.Point(495, 345);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mercuryImage
+            // 
+            this.mercuryImage.BackColor = System.Drawing.Color.Transparent;
+            this.mercuryImage.Image = global::Solar_system_WinForms.Properties.Resources.Mercury1;
+            this.mercuryImage.Location = new System.Drawing.Point(540, 322);
+            this.mercuryImage.Name = "mercuryImage";
+            this.mercuryImage.Size = new System.Drawing.Size(14, 14);
+            this.mercuryImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mercuryImage.TabIndex = 1;
+            this.mercuryImage.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Solar_system_WinForms.Properties.Resources.background;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(490, 700);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // venusImage
+            // 
+            this.venusImage.BackColor = System.Drawing.Color.Transparent;
+            this.venusImage.Image = global::Solar_system_WinForms.Properties.Resources.Venus;
+            this.venusImage.Location = new System.Drawing.Point(540, 296);
+            this.venusImage.Name = "venusImage";
+            this.venusImage.Size = new System.Drawing.Size(19, 19);
+            this.venusImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.venusImage.TabIndex = 3;
+            this.venusImage.TabStop = false;
+            // 
+            // earthImage
+            // 
+            this.earthImage.BackColor = System.Drawing.Color.Transparent;
+            this.earthImage.Image = global::Solar_system_WinForms.Properties.Resources.Earth;
+            this.earthImage.Location = new System.Drawing.Point(540, 269);
+            this.earthImage.Name = "earthImage";
+            this.earthImage.Size = new System.Drawing.Size(23, 23);
+            this.earthImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.earthImage.TabIndex = 4;
+            this.earthImage.TabStop = false;
+            // 
+            // marsImage
+            // 
+            this.marsImage.BackColor = System.Drawing.Color.Transparent;
+            this.marsImage.Image = global::Solar_system_WinForms.Properties.Resources.Mars;
+            this.marsImage.Location = new System.Drawing.Point(540, 244);
+            this.marsImage.Name = "marsImage";
+            this.marsImage.Size = new System.Drawing.Size(18, 18);
+            this.marsImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.marsImage.TabIndex = 5;
+            this.marsImage.TabStop = false;
+            // 
+            // jupiterImage
+            // 
+            this.jupiterImage.BackColor = System.Drawing.Color.Transparent;
+            this.jupiterImage.Image = global::Solar_system_WinForms.Properties.Resources.Jupiter;
+            this.jupiterImage.Location = new System.Drawing.Point(540, 171);
+            this.jupiterImage.Name = "jupiterImage";
+            this.jupiterImage.Size = new System.Drawing.Size(50, 50);
+            this.jupiterImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.jupiterImage.TabIndex = 6;
+            this.jupiterImage.TabStop = false;
+            // 
+            // saturnImage
+            // 
+            this.saturnImage.BackColor = System.Drawing.Color.Transparent;
+            this.saturnImage.Image = global::Solar_system_WinForms.Properties.Resources.Saturn;
+            this.saturnImage.Location = new System.Drawing.Point(540, 113);
+            this.saturnImage.Name = "saturnImage";
+            this.saturnImage.Size = new System.Drawing.Size(40, 40);
+            this.saturnImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.saturnImage.TabIndex = 7;
+            this.saturnImage.TabStop = false;
+            this.saturnImage.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // uranusImage
+            // 
+            this.uranusImage.BackColor = System.Drawing.Color.Transparent;
+            this.uranusImage.Image = global::Solar_system_WinForms.Properties.Resources.Uranus;
+            this.uranusImage.Location = new System.Drawing.Point(540, 66);
+            this.uranusImage.Name = "uranusImage";
+            this.uranusImage.Size = new System.Drawing.Size(28, 28);
+            this.uranusImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.uranusImage.TabIndex = 8;
+            this.uranusImage.TabStop = false;
+            this.uranusImage.Click += new System.EventHandler(this.uranusImage_Click);
+            // 
+            // neptunImage
+            // 
+            this.neptunImage.BackColor = System.Drawing.Color.Transparent;
+            this.neptunImage.Image = global::Solar_system_WinForms.Properties.Resources.Neptune;
+            this.neptunImage.Location = new System.Drawing.Point(540, 36);
+            this.neptunImage.Name = "neptunImage";
+            this.neptunImage.Size = new System.Drawing.Size(24, 24);
+            this.neptunImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.neptunImage.TabIndex = 9;
+            this.neptunImage.TabStop = false;
+            this.neptunImage.Click += new System.EventHandler(this.neptunImage_Click);
+            // 
+            // plutonImage
+            // 
+            this.plutonImage.BackColor = System.Drawing.Color.Transparent;
+            this.plutonImage.Image = global::Solar_system_WinForms.Properties.Resources.Pluto;
+            this.plutonImage.Location = new System.Drawing.Point(540, 6);
+            this.plutonImage.Name = "plutonImage";
+            this.plutonImage.Size = new System.Drawing.Size(9, 9);
+            this.plutonImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.plutonImage.TabIndex = 10;
+            this.plutonImage.TabStop = false;
+            // 
+            // moonImage
+            // 
+            this.moonImage.BackColor = System.Drawing.Color.Transparent;
+            this.moonImage.Image = global::Solar_system_WinForms.Properties.Resources.Pluto;
+            this.moonImage.Location = new System.Drawing.Point(522, 275);
+            this.moonImage.Name = "moonImage";
+            this.moonImage.Size = new System.Drawing.Size(9, 9);
+            this.moonImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moonImage.TabIndex = 11;
+            this.moonImage.TabStop = false;
+            // 
+            // phobosImage
+            // 
+            this.phobosImage.BackColor = System.Drawing.Color.Transparent;
+            this.phobosImage.Image = global::Solar_system_WinForms.Properties.Resources.Pluto;
+            this.phobosImage.Location = new System.Drawing.Point(522, 248);
+            this.phobosImage.Name = "phobosImage";
+            this.phobosImage.Size = new System.Drawing.Size(9, 9);
+            this.phobosImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.phobosImage.TabIndex = 12;
+            this.phobosImage.TabStop = false;
+            // 
+            // deimosImage
+            // 
+            this.deimosImage.BackColor = System.Drawing.Color.Transparent;
+            this.deimosImage.Image = global::Solar_system_WinForms.Properties.Resources.Pluto;
+            this.deimosImage.Location = new System.Drawing.Point(569, 248);
+            this.deimosImage.Name = "deimosImage";
+            this.deimosImage.Size = new System.Drawing.Size(9, 9);
+            this.deimosImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.deimosImage.TabIndex = 13;
+            this.deimosImage.TabStop = false;
+            // 
+            // ioImage
+            // 
+            this.ioImage.BackColor = System.Drawing.Color.Transparent;
+            this.ioImage.Image = global::Solar_system_WinForms.Properties.Resources.Pluto;
+            this.ioImage.Location = new System.Drawing.Point(514, 191);
+            this.ioImage.Name = "ioImage";
+            this.ioImage.Size = new System.Drawing.Size(9, 9);
+            this.ioImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ioImage.TabIndex = 14;
+            this.ioImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Solar_system_WinForms.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1064, 741);
+            this.Controls.Add(this.ioImage);
+            this.Controls.Add(this.deimosImage);
+            this.Controls.Add(this.phobosImage);
+            this.Controls.Add(this.moonImage);
+            this.Controls.Add(this.plutonImage);
+            this.Controls.Add(this.neptunImage);
+            this.Controls.Add(this.uranusImage);
+            this.Controls.Add(this.jupiterImage);
+            this.Controls.Add(this.marsImage);
+            this.Controls.Add(this.earthImage);
+            this.Controls.Add(this.venusImage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mercuryImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.saturnImage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mercuryImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.venusImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.earthImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marsImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jupiterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturnImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uranusImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.neptunImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plutonImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moonImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phobosImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deimosImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ioImage)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox mercuryImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox venusImage;
+        private System.Windows.Forms.PictureBox earthImage;
+        private System.Windows.Forms.PictureBox marsImage;
+        private System.Windows.Forms.PictureBox jupiterImage;
+        private System.Windows.Forms.PictureBox saturnImage;
+        private System.Windows.Forms.PictureBox uranusImage;
+        private System.Windows.Forms.PictureBox neptunImage;
+        private System.Windows.Forms.PictureBox plutonImage;
+        private System.Windows.Forms.PictureBox moonImage;
+        private System.Windows.Forms.PictureBox phobosImage;
+        private System.Windows.Forms.PictureBox deimosImage;
+        private System.Windows.Forms.PictureBox ioImage;
     }
 }
 
--- Solar system WinForms/Form1.cs
+++ Solar system WinForms/Form1.cs
@@ -16,5 +16,104 @@ namespace Solar_system_WinForms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Planet mercury = new Planet(mercuryImage, Height / 2 - mercuryImage.Location.Y, 4.75);
+            Planet venus = new Planet(venusImage, Height / 2 - venusImage.Location.Y, 5.48);
+            Planet earth = new Planet(earthImage, Height / 2 - earthImage.Location.Y, 6.84);
+            Planet mars = new Planet(marsImage, Height / 2 - marsImage.Location.Y, 9.67);
+            Planet jupiter = new Planet(jupiterImage, Height / 2 - jupiterImage.Location.Y, 13.07);
+            Planet saturn = new Planet(saturnImage, Height / 2 - saturnImage.Location.Y, 24.13);
+            Planet uranus = new Planet(uranusImage, Height / 2 - uranusImage.Location.Y, 29.76);
+            Planet neptun = new Planet(neptunImage, Height / 2 - neptunImage.Location.Y, 35.02);
+            Planet pluton = new Planet(plutonImage, Height / 2 - plutonImage.Location.Y, 47.87);
+            Satelite moon = new Satelite(moonImage, earthImage.Location.X + earthImage.Width / 2 - moonImage.Location.X + moonImage.Width / 2, 3);
+            Satelite phobos = new Satelite(phobosImage, marsImage.Location.X + marsImage.Width / 2 - phobosImage.Location.X + phobosImage.Width / 2, 3);
+            Satelite deimos = new Satelite(deimosImage, marsImage.Location.X + marsImage.Width / 2 - deimosImage.Location.X + deimosImage.Width / 2, 4);
+            Satelite io = new Satelite(ioImage, jupiterImage.Location.X + jupiterImage.Width / 2 - ioImage.Location.X + ioImage.Width / 2, 2);
+            do
+            {
+                mercury.Move();
+                venus.Move();
+                earth.Move();
+                moon.Move(earth);
+                mars.Move();
+                phobos.Move(mars);
+                deimos.Move(mars);
+                jupiter.Move();
+                io.Move(jupiter);
+                saturn.Move();
+                uranus.Move();
+                neptun.Move();
+                pluton.Move();
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1);
+            } while (true);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uranusImage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void neptunImage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    class Planet
+    {
+        protected double speed;
+        protected int distanceToSun;
+        protected double angle;
+        protected PictureBox imageObject;
+        Point startLocation;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public Planet(PictureBox imagePlanet, int rotateRadius, double speed)
+        {
+            this.speed = speed;
+            angle = -190;
+            distanceToSun = rotateRadius;
+            imageObject = imagePlanet;
+            startLocation = imageObject.Location;
+        }
+        public void Move()
+        {
+            angle += Math.PI / speed;
+            X = 540 + (int)(distanceToSun * Math.Cos(angle));
+            Y = 390 + (int)(distanceToSun * Math.Sin(angle));
+            imageObject.Location = new Point(X, Y);
+
+        }
+    }
+
+    class Satelite : Planet 
+    {
+        public Satelite (PictureBox imagePlanet, int rotateRadius, double speed) 
+            : base (imagePlanet, rotateRadius, speed)
+        { }
+        public void Move(Planet planet)
+        {
+            angle += Math.PI / speed;
+            X = planet.X + (int)(distanceToSun * Math.Cos(angle));
+            Y = planet.Y + (int)(distanceToSun * Math.Sin(angle));
+            imageObject.Location = new Point(X, Y);
+        }
     }
 }
